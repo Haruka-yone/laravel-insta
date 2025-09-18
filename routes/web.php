4566c506ef397fdbc1fbf971b8406996af17c579
chimchim <kredoteacher.oswdelosreyes@gmail.com>
2025-09-18 10:46:09 +0800

    shem comment

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\Admin\UsersController;
 
 Auth::routes();
 
-Route::group(['middleware' => 'auth'], function(){
+Route::group(['middleware' => 'auth'], function(){//SHEM
     Route::get('/', [HomeController::class, 'index'])->name('index');
     Route::get('/people', [HomeController::class, 'search'])->name('search');
 
