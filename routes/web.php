b06a6396b63cdc553039e6b4e433c485d967740b
yuumi0105 <one.uk.yu@gmail.com>
2025-09-18 10:46:35 +0800

    yuumi comment

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\Admin\UsersController;
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function(){
+    //YUUMI
     Route::get('/', [HomeController::class, 'index'])->name('index');
     Route::get('/people', [HomeController::class, 'search'])->name('search');
 
