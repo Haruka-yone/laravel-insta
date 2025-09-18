7a946b6e188d60c23f763e4fb69ec32c9b8e5797
chimchim <kredoteacher.oswdelosreyes@gmail.com>
2025-09-18 10:56:34 +0800

    adding shem

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\Admin\UsersController;
 Auth::routes();
 // haru
 Route::group(['middleware' => 'auth'], function(){
-    Route::get('/', [HomeController::class, 'index'])->name('index');
+    Route::get('/', [HomeController::class, 'index'])->name('index');//SHEM
     Route::get('/people', [HomeController::class, 'search'])->name('search');
 
     Route::group(['prefix' => 'admin','as' => 'admin.', 'middleware' => 'admin'], function(){
